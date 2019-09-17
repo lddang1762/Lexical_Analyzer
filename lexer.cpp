2b89811fa21955cea246a4a029be6353705d367c
lddang1762 <lddang1762@csu.fullerton.edu>
2019-09-17 00:05:02 -0700

    Reads file through char loop.

--- lexer.cpp
+++ lexer.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+ifstream myfile;
+
 void lexer();
+void readfile();
 
 int main(int argc, char* argv[]){
-  cout << "Hello World\n";
+  readfile();
+  lexer();
   return 0;
 }
 
 void lexer(){
-  
+  bool tokenFound = false;
+  string line;
+  char c;
+  do{
+    myfile.get(c);
+    if(!myfile.eof())
+      cout << c;
+    //getline(myfile, line);
+    //cout << line << endl;
+    //if(myfile.eof()){ cout << "END\n"; }
+  }while ( !(tokenFound || myfile.eof() ) );
+}
+
+void readfile(){
+  string fname = "";
+  cout << "Enter file name: ";
+  cin >> fname;
+  myfile.open(fname);
 }
