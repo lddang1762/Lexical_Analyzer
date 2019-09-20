b00703ca79f0ed7129a836fef16742aea96e2672
lddang1762 <lddang1762@csu.fullerton.edu>
2019-09-20 01:13:06 -0700

    Now recognizes integers. Fully working iteration finished.

--- lexer.cpp
+++ lexer.cpp
@@ -14,6 +14,7 @@ vector<char> operators = {'*', '+', '-', '=', '/', '>', '<', '%'};
 
 void lexer();
 void readfile();
+void processToken(string &token, char c);
 bool isTerminating(char c);
 bool isOperator(char c);
 bool isSeparator(char c);
@@ -28,19 +29,31 @@ int main(int argc, char* argv[]){
 
 void lexer(){
   string token = "";
+  bool realNum = true;
   char c;
   do{
     inFile.get(c);
     if(isTerminating(c)){
       if(c == '!'){
         inFile.get(c);
-        while(c != '!'){
+        while(c != '!' && c != '\n'){
           inFile.get(c);
         }
       }
       else if(isOperator(c)){
-        token += c;
-        printf("%-15s %-25s\n", "operator", token.c_str());
+        if(token.length() == 0){
+          token += c;
+          printf("%-15s %-25s\n", "operator", token.c_str());
+          token = "";
+          realNum = true;
+        }
+        else{
+          processToken(token, c);
+          realNum = true;
+        }
+      }
+      else if(c == ' ' && realNum && token.length() != 0){
+        printf("%-15s %-25s\n", "real", token.c_str());
         token = "";
       }
       else if(isSeparator(c)){
@@ -48,23 +61,23 @@ void lexer(){
           token += c;
           printf("%-15s %-25s\n", "separator", token.c_str());
           token = "";
+          realNum = true;
+        }
+        else if(realNum){
+          if(c == '.'){
+            token += c;
+          }
         }
         else{
-          if(isKeyword(token)){
-            printf("%-15s %-25s\n", "keyword", token.c_str());
-          }
-          else{
-            printf("%-15s %-25s\n", "identifier", token.c_str());
-          }
-          token = "";
-          token += c;
-          printf("%-15s %-25s\n", "separator", token.c_str());
-          token = "";
+          processToken(token, c);
+          realNum = true;
         }
       }
     }
-    //need to implement states and table to know if number or identifier
     if(isalnum(c) || c == '$'){
+      if(c != '.' && !isdigit(c)){
+        realNum = false;
+      }
       token += c;
     }
     else if((c == ' ' || c == '\n') && (token.length() != 0)){
@@ -75,17 +88,18 @@ void lexer(){
         printf("%-15s %-25s\n", "identifier", token.c_str());
       }
       token = "";
+      realNum = true;
     }
   }while ( !inFile.eof() );
 }
 
 void readfile(){
   string fname = "";
-  //cout << "Enter file name: ";
-  //cin >> fname;
+  cout << "Enter file name: ";
+  cin >> fname;
   try{
-    //inFile.open(fname);
-    inFile.open("code.txt");
+    inFile.open(fname);
+    //inFile.open("code.txt");
     if(!inFile.is_open()){
       string err = "Error: File failed to open.\n";
       throw err;
@@ -95,6 +109,19 @@ void readfile(){
     cout << e;
     exit(0);
   }
+}
+
+void processToken(string &token, char c){
+  if(isKeyword(token)){
+    printf("%-15s %-25s\n", "keyword", token.c_str());
+  }
+  else{
+    printf("%-15s %-25s\n", "identifier", token.c_str());
+  }
+  token = "";
+  token += c;
+  printf("%-15s %-25s\n", "separator", token.c_str());
+  token = "";
 }
 
 bool isTerminating(char c){
